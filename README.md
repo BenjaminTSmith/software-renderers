432260dba452922c898047d161c5640e530d3b65
Ben Smith <smithbenjamin@ufl.edu>
2024-11-14 15:46:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Software Renderers
+Two software renderers with similar feature sets (model loading and scenes). One is a ray tracer
+and the other is a rasterizer.
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main() {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return 0;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window = glfwCreateWindow(1000, 500, "Hello World", NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return 0;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return 0;
+    }
+
+    glClearColor(135 / 255.f, 206 / 255.f, 235 / 255.f, 1);
+    while (!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, true);
+        }
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
