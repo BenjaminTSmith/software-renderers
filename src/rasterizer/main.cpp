1e461e8afad0fa72deaa26c6066eee30d8fbf994
ufjustinoh <justinoh1227@gmail.com>
2024-12-04 21:31:29 -0500

    hello

--- src/rasterizer/main.cpp
+++ src/rasterizer/main.cpp
@@ -11,6 +11,10 @@
 
 #include "rasterizer.h"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 static void framebuffer_size_callback(GLFWwindow* window, int new_width, int new_height) {
     // NOTE(Ben): not sure if we should do integral or decimal scaling. leave decimal for now.
     // just float to int below to change back
@@ -20,6 +24,11 @@ static void framebuffer_size_callback(GLFWwindow* window, int new_width, int new
     int x = (new_width - width * scale) / 2;
     int y = (new_height - height * scale) / 2;
     glViewport(x, y, width * scale, height * scale);
+}
+
+constexpr double pi = 3.141592653589793238462643383;
+static double radians(double degrees) {
+    return degrees * (pi / 180.0);
 }
 
 Camera camera;
@@ -38,9 +47,9 @@ static void cursor_pos_callback(GLFWwindow* window, double x, double y) {
         last_y = y;
         first_mouse = false;
     }
-  
+
     double xoffset = x - last_x;
-    double yoffset = y - last_y; 
+    double yoffset = y - last_y;
     last_x = x;
     last_y = y;
 
@@ -58,11 +67,12 @@ static void cursor_pos_callback(GLFWwindow* window, double x, double y) {
         pitch = -89.0f;
     }
 
-    /*camera_direction.x = cos(radians(yaw)) * cos(radians(pitch));
+    Vec3 camera_direction;
+    camera_direction.x = cos(radians(yaw)) * cos(radians(pitch));
     camera_direction.y = sin(radians(pitch));
     camera_direction.z = sin(radians(yaw)) * cos(radians(pitch));
     camera_direction = normalize(camera_direction);
-    right = cross(camera_direction, up);*/
+    camera.direction = normalize(camera_direction);
 }
 
 static std::string read_file(const std::string& filepath) {
@@ -73,7 +83,7 @@ static std::string read_file(const std::string& filepath) {
     std::string file = "";
     std::string line;
     while (std::getline(filestream, line)) {
-        file += line + '\n';   
+        file += line + '\n';
     }
     filestream.close();
     return file;
@@ -83,6 +93,12 @@ static inline void update_framebuffer(const Color framebuffer[]) {
     // TODO(Ben): possibly switch to two textures and see if performance is better
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, framebuffer);
 }
+
+static const int max_models = 100;
+static int model_count = 0;
+static int selected_model_index = -1; //not selected mean "-1"
+const int max_filename_length = 256;
+static char file_name[max_filename_length] = "";
 
 int main() {
     GLFWwindow* window;
@@ -120,13 +136,13 @@ int main() {
     // NOTE(Ben): texture coordinates are flipped so that the first index into the framebuffer is
     // the top left corner.
     float vertices[] = {
-        -1.f, 1.f, 0.f, 0.f, // top left
-        1.f, 1.f, 1.f, 0.f, // top right
-        -1.f, -1.f, 0.f, 1.f, // bottom left
+            -1.f, 1.f, 0.f, 0.f, // top left
+            1.f, 1.f, 1.f, 0.f, // top right
+            -1.f, -1.f, 0.f, 1.f, // bottom left
 
-        1.f, 1.f, 1.f, 0.f, // top right
-        1.f, -1.f, 1.f, 1.f, // bottom right
-        -1.f, -1.f, 0.f, 1.f // bottom left
+            1.f, 1.f, 1.f, 0.f, // top right
+            1.f, -1.f, 1.f, 1.f, // bottom right
+            -1.f, -1.f, 0.f, 1.f // bottom left
     };
 
     unsigned int vbo;
@@ -184,18 +200,31 @@ int main() {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
                  0, GL_RGB, GL_UNSIGNED_BYTE, framebuffer);
     // Don't know if needed yet
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     glClearColor(0, 0, 0, 0);
 
-    for (int i = 0; i < width * height; i++) {
-        framebuffer[i] = Color(0, 0, 0);
-    }
+    // IMGUI STARTS HERE
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true); // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
+
+    Model models[max_models];
+    // james said we could maybe put a create_model function? idk though he can do it
+    models[model_count].mesh = load_mesh("head.obj");
+    models[model_count].position = Vec3(0.5, 0, 0);
+    model_count++;
 
     camera.position = Vec3(0, 0, 3);
+    camera.direction = Vec3(0, 0, -1);
     while (!glfwWindowShouldClose(window)) {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, true);
@@ -212,13 +241,82 @@ int main() {
         } if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
             camera.position.y -= 0.1;
         }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        {
+            ImGui::Begin("Scene Control");
+            ImGui::InputText("Model File", file_name, max_filename_length);
+            if (ImGui::Button("Add Model")) {
+                if (model_count < max_models) {
+                    Mesh mesh = load_mesh(file_name);
+                    if (mesh.vertices.empty()) {
+                        ImGui::Text("Failed to load model: %s", file_name);
+                    } else {
+                        models[model_count].mesh = mesh;
+                        models[model_count].position = Vec3(0.0f, 0.0f, 0.0f);
+                        model_count++;
+                    }
+                } else {
+                    ImGui::Text("Max num of models");
+                }
+            }
+            ImGui::Text("Number of heads: %d", model_count);
+            ImGui::Separator();
+            ImGui::Text("Models in Scene:");
+
+            for (int i = 0; i < model_count; ++i) {
+                char label[32];
+                snprintf(label, sizeof(label), "Model %d", i);
+                if (ImGui::Selectable(label, selected_model_index == i)) {
+                    selected_model_index = i;
+                }
+            }
+
+            if (selected_model_index >= 0 && selected_model_index < model_count) {
+                ImGui::Separator();
+                ImGui::Text("Edit Model %d", selected_model_index);
+
+                Model& selected_model = models[selected_model_index];
+                float position[3] = {
+                        static_cast<float>(selected_model.position.x),
+                        static_cast<float>(selected_model.position.y),
+                        static_cast<float>(selected_model.position.z)
+                };
+                if (ImGui::InputFloat3("Edit Position", position)) {
+                    selected_model.position.x = position[0];
+                    selected_model.position.y = position[1];
+                    selected_model.position.z = position[2];
+                }
+
+                if (ImGui::Button("Remove Model")) {
+                    for (int i = selected_model_index; i < model_count - 1; ++i) {
+                        models[i] = models[i + 1];
+                    }
+                    model_count--;
+                    selected_model_index = -1;
+                }
+
+            }
+
+            ImGui::End();
+        }
+
+
+
         glClear(GL_COLOR_BUFFER_BIT);
         // render here:
-        render(framebuffer, camera);
+        render(framebuffer, camera, models, model_count);
         update_framebuffer(framebuffer);
         // TODO(Ben): Possibly change to an index buffer if need be.
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
+
+        // IMGUI IN RENDER LOOP HERE
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        //
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -227,6 +325,9 @@ int main() {
     glDeleteShader(fragment_shader);
     glfwTerminate();
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
     delete[] framebuffer;
     return 0;
-}
